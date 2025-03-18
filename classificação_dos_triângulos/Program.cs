02da1d140c4f01b4120d5a8dc677070951986447
leandroJader <ldandro>
2025-03-18 15:27:58 -0300

    Add project files.

---
+++ classificação_dos_triângulos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace classificação_dos_triângulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
