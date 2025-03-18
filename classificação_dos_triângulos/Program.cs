db016b215f440a99481dcc6fa0b646490cde1680
leandroJader <ldandro>
2025-03-18 15:41:32 -0300

    implementada versão inicial

--- classificação_dos_triângulos/Program.cs
+++ classificação_dos_triângulos/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+
+
+
+
+
+
+
+                Console.Clear();
+                Console.WriteLine("=========================") ;
+                Console.WriteLine("Classificador de triangulos");
+                Console.WriteLine("=========================");
+                
+                Console.WriteLine("informe o lado x do triangulo");
+
+                int ladox = Convert.ToInt32(Console.ReadLine());
+                Console.Write("informe o lado y");
+                int ladoy = Convert.ToInt32(Console.ReadLine());
+                Console.Write("informe o lado z");
+                int ladoz = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("============================");
+                Console.WriteLine("exibir valores ");
+                Console.WriteLine("lado x "+ladox);
+                Console.WriteLine("lado y "+ ladoy);
+                Console.WriteLine("lado z"+ ladoz);
+
+                Console.Write("==========================");
+
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+
+
+
+
+
+
+                Console.Write("deseja continuar s/n");
+                if (opcaoContinuar != "S")
+                {
+                    break;
+                }
+            }
         }
     }
 }
