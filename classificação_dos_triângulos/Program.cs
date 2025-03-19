04c5b9c48ac730be81c2d6da64420fefdede52a2
leandroJader <ldandro>
2025-03-19 08:52:14 -0300

    implementada classe estatica

--- classificação_dos_triângulos/Program.cs
+++ classificação_dos_triângulos/Program.cs
@@ -1,108 +1,84 @@
-﻿using System;
-
-namespace classificação_dos_triângulos
+﻿
+namespace classificação_dos_triângulos;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            while (true)
+            ExibirMenu();
+
+            int ladox = LerLados("x");
+            int ladoy = LerLados("y");
+            int ladoz = LerLados("z");
+
+            ExibirValoresInformados(ladox,ladoy,ladoz);
+           
+         
+            Console.WriteLine("==========================");
+
+            bool MedidasFormamTrianguloValido = classificadorDeTriangulos.MedidasFormamTrianguloValido(ladox, ladoy, ladoz);
+
+            if (MedidasFormamTrianguloValido)
             {
-                ExibirMenu();
 
-                int ladox = LerLados("x");
-                int ladoy = LerLados("y");
-                int ladoz = LerLados("z");
+                string TipoTriangulo = classificadorDeTriangulos.ClassificarTriangulo(ladox, ladoy, ladoz);
 
-                ExibirValoresInformados(ladox,ladoy,ladoz);
-               
-                //verificar validade do triangulo
-                Console.Write("==========================");
-                
-                bool medidasTrianguloValida =
-                    ladox+ladoy>ladoz &&
-                    ladox+ladoz>ladoy &&
-                    ladoy +ladoz>ladox;
-
-                if (medidasTrianguloValida == true)
-                {
-
-                    string TipoTRiangulo = "nao classificado";
+                    Console.WriteLine($"o triangulo e {TipoTriangulo}"); 
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("======================================");
+                Console.WriteLine( " os valores informados nao formam um triangulo");
+                Console.ResetColor();
+            }
 
 
-                    if (ladox == ladoy && ladoy == ladoz)
-                        TipoTRiangulo = "equilatero";
-
-
-
-                    else if (ladox != ladoy && ladoy != ladoz && ladox != ladoz)
-                        TipoTRiangulo = "escaleno";
-
-
-                    else 
-                        TipoTRiangulo = "isosceles";
-
-                        Console.WriteLine($"o triangulo e {TipoTRiangulo}"); 
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("======================================");
-                    Console.WriteLine( " os valores informados nao formam um triangulo");
-                    Console.ResetColor();
-                }
-
-
-                Console.Write("deseja continuar S/N");
-                 string opcaoContinuar = Console.ReadLine()!.ToUpper();
-                if (opcaoContinuar != "S")
-                {
-                    break;
-                }
+            Console.Write("deseja continuar S/N");
+             string opcaoContinuar = Console.ReadLine()!.ToUpper();
+            if (opcaoContinuar != "S")
+            {
+                break;
             }
         }
+    }
 
-        public static void ExibirMenu()
+    public static void ExibirMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("=========================");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Classificador de triangulos");
+        Console.ResetColor();
+        Console.WriteLine("=========================");
+    }
+   static int LerLados (string nomelado)
+    {
+
+        int valor;
+
+        while (true)
         {
-            Console.Clear();
-            Console.WriteLine("=========================");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Classificador de triangulos");
-            Console.ResetColor();
-            Console.WriteLine("=========================");
-        }
-       static int LerLados (string nomelado)
-        {
+            Console.WriteLine($"informe o lado {nomelado}");
 
-            int valor;
-
-            while (true)
+            bool consegiuConverter = int.TryParse(Console.ReadLine(), out valor);
+            if (consegiuConverter)
             {
-                Console.WriteLine($"informe o lado {nomelado}");
-
-                bool consegiuConverter = int.TryParse(Console.ReadLine(), out valor);
-                if (consegiuConverter)
-                {
-                    break;
-                }
+                break;
             }
-                return valor;
         }
-        static void ExibirValoresInformados(int ladox,int ladoy,int ladoz)
-        {
+            return valor;
+    }
+    static void ExibirValoresInformados(int ladox,int ladoy,int ladoz)
+    {
 
-            Console.WriteLine("============================");
-            Console.WriteLine("exibir valores ");
-            Console.WriteLine("lado x " + ladox);
-            Console.WriteLine("lado y " + ladoy);
-            Console.WriteLine("lado z " + ladoz);
-
-        }
-
-
-
-
-
+        Console.WriteLine("============================");
+        Console.WriteLine("exibir valores ");
+        Console.WriteLine("lado x " + ladox);
+        Console.WriteLine("lado y " + ladoy);
+        Console.WriteLine("lado z " + ladoz);
 
     }
+
 }
---
+++ classificação_dos_triângulos/classificadorDeTriangulos.cs
@@ -0,0 +1,36 @@
+﻿
+namespace classificação_dos_triângulos;
+
+public static class classificadorDeTriangulos
+{
+    public static bool MedidasFormamTrianguloValido(int ladox, int ladoy, int ladoz)
+    {
+
+        bool medidasTrianguloValida =
+             ladox + ladoy > ladoz &&
+             ladox + ladoz > ladoy &&
+             ladoy + ladoz > ladox;
+        return medidasTrianguloValida;
+    }
+     public  static string ClassificarTriangulo(int ladox, int ladoy, int ladoz)
+    {
+        string TipoTriangulo = "nao classificado";
+
+
+        if (ladox == ladoy && ladoy == ladoz)
+            TipoTriangulo = "Equilátero";
+
+
+
+        else if (ladox != ladoy && ladoy != ladoz && ladox != ladoz)
+            TipoTriangulo = "Escaleno";
+
+
+        else
+            TipoTriangulo = "isósceles";
+
+        return TipoTriangulo;
+    }
+
+
+}
